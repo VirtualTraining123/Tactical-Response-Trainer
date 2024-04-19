763dcddcbbe4e1715e0396a1c40ee3d928c2b68e
GabVit <gabriel.n.vitelli@gmail.com>
2024-04-19 17:34:01 -0300

    Versión casi final (creo)
    -Ya hay impactos de bala presentes en el escenario, utilizar el cubo que dispara como testeador si no se tienen los lentes.
    -Los objetivos sangran al recibir un disparo.
    -Se tiene el efecto de sangre al recibir un disparo, que te lleva al escenario de game over, para probar esto sin lentes se debe activar es Enemy Spawner (2) que genera 1 enemigo en frente.
    -Al eliminar a los enemigos se pasa a la pantalla de victoria automaticamente, quizas se puede agregar un efecto visual antes del salto repentino, es un punto a charlar porque no se me ocurre algun buen efecto.
    
    Faltante:
    -Verificar que realmente se cambiaron los controles por manos en el escenario de juego
    -Corregir posición de controles en los escenarios previos y posteriores al nivel (por algún motivo están girados 180° en 2 ejes jaja)

--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -7,13 +7,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Pistol : Weapon
 {
     [SerializeField] private Projectile bulletPrefab;
-    
-
+    //////////////////////////
+    [SerializeField] private float debugRayDuration = 2f;
+    public GameObject bulletHolePrefab;
     
     protected override void StartShooting(XRBaseInteractor interactor)
     {
         base.StartShooting(interactor);
-        
+        DrawDebugRaycast();
         Shoot();
 
     }
@@ -22,6 +23,15 @@ public class Pistol : Weapon
     {
 
         base.Shoot();
+
+         RaycastHit hit;
+        if (Physics.Raycast(bulletSpawn.position, bulletSpawn.forward, out hit))
+        {
+            // Instanciar el objeto bullet hole en el punto de impacto
+            InstantiateBulletHole(hit.point, hit.normal, hit.collider.transform);
+        }
+
+
         Projectile projectileInstance = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         projectileInstance.Init(this);
         projectileInstance.Launch();
@@ -34,4 +44,23 @@ public class Pistol : Weapon
     {
         base.StopShooting(interactor);
     }
+
+/////////////////////////////////////////////////////////
+///
+     private void InstantiateBulletHole(Vector3 position, Vector3 normal, Transform parent)
+    {
+        // Calcular la rotación necesaria para alinear el eje hacia arriba con la normal de la superficie
+    Quaternion rotation = Quaternion.FromToRotation(Vector3.up, normal);
+
+    // Instanciar el objeto bullet hole en el punto de impacto con la rotación adecuada y como hijo del objeto impactado
+    GameObject bulletHoleInstance = Instantiate(bulletHolePrefab, position, rotation, parent);
+    }
+
+    private void DrawDebugRaycast()
+    {
+        // Dibujar el rayo de debug
+        Debug.DrawRay(bulletSpawn.position, bulletSpawn.forward * 100f, Color.yellow, debugRayDuration);
+    }
+
+    
 }
