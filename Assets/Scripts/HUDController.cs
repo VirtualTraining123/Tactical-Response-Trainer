847099f824d4e034b069f13428ca0b59bd5be0df
Federico Williamson - WGPC5 <federicowilliamson@hotmail.co.uk>
2024-09-06 21:37:30 -0300

    Refactor de HUDController.cs

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -1,27 +1,24 @@
 using TMPro;
 using UnityEngine;
 
-public class HUDController : MonoBehaviour
-{
-    public TextMeshProUGUI Enemyremaining; //no lo arreglo porque crashea rider
-    public TextMeshProUGUI CiviliansInjured;
-    public TextMeshProUGUI Safetylock;
-    public TextMeshProUGUI bullets;
-    public TextMeshProUGUI timeText;
-    private Evaluator evaluator;
+public class HUDController : MonoBehaviour {
+  public TextMeshProUGUI enemyRemaining; //no lo arreglo porque crashea rider
+  public TextMeshProUGUI civiliansInjured;
+  public TextMeshProUGUI safetyLock;
+  public TextMeshProUGUI bullets;
+  public TextMeshProUGUI timeText;
+  private Evaluator evaluator;
 
-    void Start()
-    {
-        evaluator = FindObjectOfType<Evaluator>();
-    }
+  void Start() {
+    evaluator = FindObjectOfType<Evaluator>();
+  }
 
-    void Update()
-    {
-        // Actualiza el texto con los valores actuales del evaluador
-        Enemyremaining.text = "Enemigos Eliminados: " + evaluator.GetEnemiesEliminated();
-        CiviliansInjured.text = "Civiles Heridos: " + evaluator.GetCiviliansInjured();
-        timeText.text = "Tiempo: " + evaluator.GetElapsedTime().ToString("F2");
-        Safetylock.text = "Seguro: " + evaluator.GetSafetyLock();
-        bullets.text = "Balas en cargador: " + evaluator.GetBullets();
-    }
+  void Update() {
+    // Actualiza el texto con los valores actuales del evaluador
+    enemyRemaining.text = "Enemigos Eliminados: " + evaluator.GetEnemiesEliminated();
+    civiliansInjured.text = "Civiles Heridos: " + evaluator.GetCiviliansInjured();
+    timeText.text = "Tiempo: " + evaluator.GetElapsedTime().ToString("F2");
+    safetyLock.text = "Seguro: " + evaluator.GetSafetyLock();
+    bullets.text = "Balas en cargador: " + evaluator.GetBullets();
+  }
 }
