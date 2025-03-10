77de8374fe9c4527c0b25af9e868057fd882d1ce
Juan Bercoff <jmbercoff@gmail.com>
2025-03-10 20:59:02 -0300

    refactor network projectile

--- Assets/Scripts/Projectiles/NetworkProjectile.cs
+++ Assets/Scripts/Projectiles/NetworkProjectile.cs
@@ -3,7 +3,8 @@ using Fusion;
 
 namespace Projectiles
 {
-    public class NetworkProjectile : Projectile {
+    public class NetworkProjectile : BaseProjectile
+    {
         [Networked] private Vector3 NetworkedVelocity { get; set; }
         [Networked] private TickTimer LifeTimer { get; set; }
 
@@ -12,59 +13,46 @@ namespace Projectiles
         public NetworkRunner Runner => _networkObject?.Runner;
         public NetworkObject NetworkObj => _networkObject;
 
-        private void Awake() {
-            _networkObject = GetComponent<NetworkObject>();
-            Debug.Log("NetworkProjectile is here??" + _networkObject);
-        }
-
-        public override void Init(Weapon weapon) {
+        public override void Init(Weapon weapon)
+        {
             base.Init(weapon);
-
-            if (Runner.IsServer) {
-                // Set up a lifetime for the projectile to avoid orphaned objects
-                LifeTimer = TickTimer.CreateFromSeconds(Runner, _maxLifetime);
-            }
+            // Set up a lifetime for the projectile to avoid orphaned objects
+            LifeTimer = TickTimer.CreateFromSeconds(Runner, _maxLifetime);
         }
 
-        public override void Launch() {
-            if (Runner.IsServer) {
-                var rb = GetComponent<Rigidbody>();
-                rb.linearVelocity = transform.forward * speed;
-                NetworkedVelocity = rb.linearVelocity;
+        public override void Launch()
+        {
+            var rb = GetComponent<Rigidbody>();
+            rb.linearVelocity = transform.forward * speed;
+            NetworkedVelocity = rb.linearVelocity;
 
-                // Spawn trail effect on all clients
-                RPC_SpawnTrail();
-            }
+            // Spawn trail effect on all clients
+            HandleTrail();
         }
 
-        public override void FixedUpdateNetwork() {
-            if (LifeTimer.Expired(Runner)) {
+        public void FixedUpdateNetwork()
+        {
+            if (LifeTimer.Expired(Runner))
+            {
                 Runner.Despawn(_networkObject);
                 return;
             }
-
-            // Non-authoritative clients should update position based on networked velocity
-            if (!Runner.IsServer) {
-                transform.position += NetworkedVelocity * Runner.DeltaTime;
-            }
+            transform.position += NetworkedVelocity * Runner.DeltaTime;
         }
 
-        protected override void HandleHit(ContactPoint contact, Transform hitTransform) {
-            base.HandleHit(contact, hitTransform);
-
-            // Despawn the networked object when it hits something (only on server)
-            if (Runner.IsServer) {
-                Runner.Despawn(_networkObject);
-            }
-        }
         // TODO
         // This is laggy, maybe the trail should be local only
         // But it's a good example of how to spawn effects on all clients
         [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-        private void RPC_SpawnTrail(){
-            if (bulletTrailPrefab == null) return;
+        protected override void HandleTrail()
+        {
             var trail = Instantiate(bulletTrailPrefab, transform.position, Quaternion.identity);
             trail.transform.SetParent(transform);
+        }
+
+        private void Awake()
+        {
+            _networkObject = GetComponent<NetworkObject>();
         }
     }
 }
