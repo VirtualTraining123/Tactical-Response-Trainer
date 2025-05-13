71f6d9e180c79286526adc80f5a6813e2d280a07
Gabriel Nicolás Vitelli <gabriel.n.vitelli@gmail.com>
2025-05-13 13:11:48 -0300

    Sonido Ambiental. Aplicación sencilla. Ver como reducir tamaño de archivo de sonido, no esta

---
+++ Assets/Scripts/AmbientSound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AmbientSound : MonoBehaviour
+{
+    protected AudioManager audioManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        audioManager = FindFirstObjectByType<AudioManager>();
+        audioManager.Request("Ambient", gameObject);
+        audioManager.Play("Ambient", gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
