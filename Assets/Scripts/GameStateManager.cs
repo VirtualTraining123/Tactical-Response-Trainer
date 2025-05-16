ccace6288ae961ceb2313885d0bffa23632377db
Federico Williamson - WGPC6 <federicowilliamson@hotmail.co.uk>
2025-05-16 17:11:01 -0300

    Align

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -4,18 +4,63 @@ using System.Linq;
 using UnityEngine;
 
 public class GameStateManager : MonoBehaviour {
+  public enum GameState {
+    Outside,
+    Inside,
+    Transition
+  }
   [SerializeField]
   private List<GameObject> OutsideMeshes;
   [SerializeField]
   private List<GameObject> InsideMeshes;
   [SerializeField]
   private List<GameObject> IntersectingMeshes;
+  [SerializeField]
+  private GameState currentGameState = GameState.Outside;
+  private GameState lastGameState = GameState.Inside;
+
 
   private void Awake() {
     // Assert IntersectingMeshes is a subset of OutsideMeshes
     if (IntersectingMeshes.Any(mesh => !OutsideMeshes.Contains(mesh))) {
       Debug.LogError("IntersectingMeshes must be a subset of OutsideMeshes");
     }
+  }
+
+  private void Update() {
+    // if (shouldGoInside()) currentGameState = GameState.Inside;
+    // else if (shouldTransition()) currentGameState = GameState.Transition;
+    // else if (shouldGoOutside()) currentGameState = GameState.Outside;
+    if (currentGameState == lastGameState) return;
+    switch (currentGameState) {
+      case GameState.Inside:
+        ToInside();
+        break;
+      case GameState.Transition:
+        ToTransition();
+        break;
+      case GameState.Outside:
+        ToOutside();
+        break;
+      default:
+        throw new ArgumentOutOfRangeException();
+    }
+    lastGameState = currentGameState;
+  }
+
+  public bool shouldGoInside() {
+    // TODO
+    return false;
+  }
+
+  public bool shouldTransition() {
+    // TODO
+    return false;
+  }
+
+  public bool shouldGoOutside() {
+    // TODO
+    return true;
   }
 
   public void DisplayOutside() {
